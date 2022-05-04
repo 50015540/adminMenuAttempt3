7484b12236988861f3b090349fdb6c90bb12bdf4
Kasaya <kledua55@gmail.com>
2022-05-04 14:37:59 +0100

    Initial commit

---
+++ app/src/main/java/com/example/adminmenuattempt3/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.adminmenuattempt3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import  android.view.View;
+import com.google.android.material.navigation.NavigationView;
+
+
+
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
